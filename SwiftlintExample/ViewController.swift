f0aa77f1861af2a66052b951f86aea39e3241c60
Yiyin Shen <yiyin.shen@anz.com>
2019-04-02 14:24:16 +1100

    Initial Commit

---
+++ SwiftlintExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftlintExample
+//
+//  Created by Yiyin Shen on 2/4/19.
+//  Copyright © 2019 Sylvia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
